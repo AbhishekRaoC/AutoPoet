651b4abedd9e6b3d5dd2a760a067ce24fd00979a
AbhishekRaoC <abhishekraochimbili@gmail.com>
2019-10-28 15:17:16 +0400

    Need to add upload to site versions

--- src/generatePoem.py
+++ src/generatePoem.py
@@ -3,9 +3,7 @@ import markovify
 
 dataIn= pd.read_csv('../src/poemDataset.csv')
 
-dataIn.head(1)
-
-text_model = markovify.NewlineText(dataIn, state_size=2)
+text_model = markovify.NewlineText(dataIn.Content, state_size=2)
 
 for i in range(5):
     print(text_model.make_sentence())
